198ad08ecd5bba8fdd82b0d4727d49dfcf60890f
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-06 09:59:44 +0100

    skeleton setup done

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+// const passport = require('passport'); TODO
+const flash = require('connect-flash');
+const session = require('express-session');
+
+const app = express();
+
+// Passport Config Add Here
+
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// Passport middleware add here
+
+app.use(flash());
+
+app.get("/", (req, res) => {
+    res.render("dashboard");
+});
+
+module.exports = app;
