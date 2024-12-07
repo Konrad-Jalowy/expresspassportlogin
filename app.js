6523477cf487b580baf08bea38566c8b7e5e455a
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-07 11:01:18 +0100

    static files setup

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ require('./passport-config')(passport);
 
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
-
+app.use(express.static('public'))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
